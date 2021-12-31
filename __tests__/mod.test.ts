834d4198c949abfb54ef7287dc9d92db8381ac4f
Igor Snitkin <snigo.ogins@gmail.com>
2021-12-31 01:24:20 +0100

    add mod function

---
+++ __tests__/mod.test.ts
@@ -0,0 +1,16 @@
+import mod from '../src/mod';
+
+test('correct modulo value for positive and negative numbers', () => {
+  expect(mod(11, 3)).toBe(2);
+  expect(mod(-1, 3)).toBe(2);
+  expect(mod(1, -45)).toBe(-44);
+  expect(mod(11, 3)).toBe(2);
+  expect(mod(-1, 3)).toBe(2);
+  expect(mod(1, -45)).toBe(-44);
+});
+
+test('NaN', () => {
+  expect(mod(1, 0)).toBeNaN();
+  expect(mod(1, Infinity)).toBeNaN();
+  expect(mod(1, NaN)).toBeNaN();
+});
