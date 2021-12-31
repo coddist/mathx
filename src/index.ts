9adb43f0a76af078546a12e54782f6da4452cdc8
Igor Snitkin <snigo.ogins@gmail.com>
2021-12-31 02:16:50 +0100

    prepare for publish

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export { default as approx } from './approx';
+export { default as getPrecision } from './getPrecision';
+export { default as mod } from './mod';
+export { default as parseNum } from './parseNum';
+export { default as random } from './random';
+export { default as round } from './round';
